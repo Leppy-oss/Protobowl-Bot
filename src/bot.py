b86405937a3d8bdc8925feb219b04f02473ed68a
ryan <tyryanosaur@gmail.com>
2023-04-03 22:26:02 -0500

    get knowledge and answers features

--- src/bot.py
+++ src/bot.py
@@ -1,3 +1,4 @@
+import unicodedata
 import selenium
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -33,11 +34,45 @@ def buzz(text):
     guess_input.send_keys(text + '\n')
     sleep(0.5) # let the guess box appear
 
+def get_knowledge(i):
+	bundle = driver.find_elements(By.CLASS_NAME, 'bundle')[i]
+	qid = bundle.get_attribute("class").split("qid-")[1].split(" ")[0]
+	if i > 0: #this feels inelegant, but works
+		raw_breadcrumb = bundle.find_element(By.CLASS_NAME, 'breadcrumb').text
+		answer = raw_breadcrumb.split("/Edit\n")[1]
+		answer = unicodedata.normalize('NFKD', answer).encode("ascii", "ignore").decode()
+		answer = answer.split("(")[0] #get only the first part of the answer, not the parenthetical note
+		answer = answer.split("[")[0] #get only the first part of the answer, not the parenthetical note
+		answer = answer.strip() #strip whitespace characters from around the answer (presumably in between the parenthetical note and the real answer)
+		answer = answer.strip(u"\u2018").strip(u"\u2019").strip(u"\u201c").strip(u"\u201d").strip("'").strip("\"") # strip all quote marks, which occasionally cause Protobowl to reject correct answers
+	else:
+		answer = ''
+	return {'qid': qid, 'answer': answer}
+
+def get_raw_breadcrumb(i):
+	return driver.find_elements(By.CLASS_NAME, 'breadcrumb')[i].text
+
+def get_breadcrumb(i):
+	return get_raw_breadcrumb(i).split("/Edit\n")[0]
+
+def get_answer(i):
+	if i > 0: #this feels inelegant, but works
+		return get_raw_breadcrumb(i).split("/Edit\n")[1]
+	else:
+		return ''
+
 print(btn.__repr__())
 
 while True:
+    '''
     btn.click()
     if (btn.should_run()):
         buzz("rock")
+    '''
+    try:
+        print (get_knowledge(0))
+        print (get_answer(1))
+    except Exception as e:
+        print('error while getting knowledge')
 
     sleep(1)
